d389f0c24539108ce2b735ae398cd33e43fc9a73
Aekavich Arunyanak <aarunyan@Aekavichs-MacBook-Pro.local>
2025-12-22 12:57:08 +0800

    ...

---
+++ main.js
@@ -0,0 +1,11 @@
+async function start() {
+  const miamiweather__promise = await fetch('https://api.weather.gov/gridpoints/MFL/110,50/forecast');
+  const miami_weather_json = await miamiweather__promise.json();
+  const miami_weather_forecast = miami_weather_json.properties.periods[0].temperature;
+  // console.log(miami_weather_forecast);
+  document.querySelector("#dynamic-weather").textContent = miami_weather_forecast;
+}
+
+start()
+
+console.log("bye")
