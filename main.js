ec7a9960f277cdc8edd6dd0a50cd6739db07f858
Aekavich Arunyanak <aarunyan@Aekavichs-MacBook-Pro.local>
2025-12-23 19:29:21 +0800

    finished dynamic pet card retrival

--- main.js
+++ main.js
@@ -13,21 +13,7 @@ async function start() {
   document.querySelector("#dynamic-weather").textContent = miami_weather_forecast;
 }
 // start(); //Uncomment if I want to retrieve Miami weather data
-
-
-/*
-Retrieve pet database from promise to json
-- await is used for waiting current instruction until complete then move to next instruction
-- await must be used with async function
-*/
-
-async function fetch_pet_data() {
-  pet_promise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json");
-  pet_json = await pet_promise.json();
-  console.log(pet_json);
-}
-// fetch_pet_data(); //Uncomment if I want to retrieve pet data
-
+// --------------------------------------------------------------------------------
 
 /*
 Update style of buttons based on current click
@@ -53,6 +39,52 @@ function click_to_update_color(event) {
   //Update active (blue color font to the button we click)
   event.currentTarget.classList.add("active");
 }
+// --------------------------------------------------------------------------------
+
+// const aek = document.querySelector(".pet-card-list");
+// console.log(aek)
+
+/*
+Retrieve pet database from promise to json
+- await is used for waiting current instruction until complete then move to next instruction
+- await must be used with async function
+*/
 
 
 
+async function fetch_pet_data() {
+  const pet_promise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json");
+  const pet_json = await pet_promise.json();
+  // console.log(pet_json);
+  const pet_card_template = document.querySelector("#pet-card-template");
+  console.log(pet_json)
+  pet_json.forEach(el => {
+    const pet_card_deep_clone = pet_card_template.content.cloneNode(true);
+    pet_card_deep_clone.querySelector("#pet-img img").src = el.photo;
+    pet_card_deep_clone.querySelector("#pet-name").textContent = el.name;
+    pet_card_deep_clone.querySelector("#pet-detail").textContent = el.description;
+    pet_card_deep_clone.querySelector("#pet-age").textContent = get_age_year(el.birthYear);
+    document.querySelector("#pet-card-list").appendChild(pet_card_deep_clone);
+    console.log(el.name)
+  })
+
+}
+
+fetch_pet_data(); //Uncomment if I want to retrieve pet data
+
+
+function get_age_year(birthYear) {
+  const current_year = new Date().getFullYear();
+  const age = current_year - birthYear;
+  if (age == 1) {
+    return `${age} year old`;
+  } else if (age > 1) {
+    return `${age} years old`;
+  } else {
+    return `less than a year`
+  }
+}
+
+
+
+// --------------------------------------------------------------------------------
